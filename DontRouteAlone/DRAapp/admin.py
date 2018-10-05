0be7054de7a194244dd39877137934120797f2ff
KinitoLuque <kinito_96@hotmail.com>
2018-10-05 20:40:13 +0200

    admin

--- DontRouteAlone/DRAapp/admin.py
+++ DontRouteAlone/DRAapp/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Usuario
+from .models import Ruta
+from .models import Localizacion
+from .models import Participantes
+
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Ruta)
+admin.site.register(Localizacion)
+admin.site.register(Participantes)
