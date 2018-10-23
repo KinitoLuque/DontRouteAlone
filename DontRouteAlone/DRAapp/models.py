c8a756015c60b0065c2896048edb7b15d5a8bfff
KinitoLuque <kinito_96@hotmail.com>
2018-10-23 16:56:37 +0200

    Extensión de usuario

--- DontRouteAlone/DRAapp/models.py
+++ DontRouteAlone/DRAapp/models.py
@@ -3,13 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Usuario(models.Model):
 	nombre = models.CharField(max_length=15)
 	email = models.EmailField(max_length=254)
 	fNac = models.DateField(auto_now=False, auto_now_add=False)
 	imagen = models.ImageField()
+	usuario = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
 
 	def __unicode__(self):
 		return self.nombre
